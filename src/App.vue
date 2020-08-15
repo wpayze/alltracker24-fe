07db2c990461d4cc8a364b58df3b6217a065a627
Wilfredo <wilfredopaizpaz@gmail.com>
2020-08-14 19:38:49 -0600

    initial 2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <Navbar />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-
-import Navbar from './components/Navbar';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    HelloWorld
   }
 }
 </script>
